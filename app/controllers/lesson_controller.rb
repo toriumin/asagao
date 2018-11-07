d1890ae3ceadd8c63f57323a2da79e50180a4f2c
toriumi <saori.toriumi.54@gmail.com>
2018-11-08 03:20:34 +0900

    Add view price

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -6,4 +6,24 @@ class LessonController < ApplicationController
     def step2
         render plain: params[:controller] + "#" + params[:action]
     end
+
+    def step3
+        rendirect_to action: "step4"
+    end
+
+    def step4
+        render plain: "step4に移動しました。"
+    end
+
+    def step5
+        flash[:notice] = "step6に移動します。"
+    end
+
+    def step6
+        render plain: flash[:notice]
+    end
+
+    def step7
+        @price = (2000 * 1.08).floor
+    end
 end
