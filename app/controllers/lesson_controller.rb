934643b19f3d3f4eebea66593ea0ce01f596c61c
toriumi <saori.toriumi.54@gmail.com>
2018-11-08 02:50:11 +0900

    Add action step2 to lesson_controller file

--- app/controllers/lesson_controller.rb
+++ app/controllers/lesson_controller.rb
@@ -2,4 +2,8 @@ class LessonController < ApplicationController
     def step1
         render plain: "こんにちは、#{params[:name]}さん"
     end
+
+    def step2
+        render plain: params[:controller] + "#" + params[:action]
+    end
 end
