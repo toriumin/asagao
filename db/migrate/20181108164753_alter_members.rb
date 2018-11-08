0af8d62bba9f984fcae902b971b804b7b3f71867
toriumi <saori.toriumi.54@gmail.com>
2018-11-09 02:03:07 +0900

    Add column alter_members to members

--- db/migrate/20181108164753_alter_members.rb
+++ db/migrate/20181108164753_alter_members.rb
@@ -1,5 +1,11 @@
 class AlterMembers < ActiveRecord::Migration[5.1]
   def change
-    add_column :members, :phone, :string
+    rename_column :members, :name, :nickname
+    change_column :members, :sex, :integer, null: false, default: 2
+  end
+
+  def down
+    change_column :members, :sex, :integer, null: false, default: 1
+    rename_column :members, :nickname, :name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108162805) do
+ActiveRecord::Schema.define(version: 20181108164753) do
 
   create_table "members", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
   end
 
 end
